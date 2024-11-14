3da50042bd477b456fae78bc74718a967f6d1ca2
Lorrayne Lima Mota <lorraynelimamota@gmail.com>
2024-11-14 09:46:06 -0300

    Add: 1st commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <img src={$logoimage} alt="Logo Studio Ghibli" />
+      <h1>Studio Ghibli</h1>
+    </header>
+  )
+}
+
+export default Header
